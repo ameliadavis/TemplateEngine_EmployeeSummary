1563a4d6380b565471f92fbbfc979733d6bd53a9
Amelia Davis <ameliaaltman22@gmail.com>
2020-03-09 22:44:31 -0600

    Tests are all running pass

--- app2.js
+++ app2.js
@@ -19,9 +19,76 @@ function newEmployeeSurvey (){
         {   
             type: "list",
             message: "What is their title?",
-            choices: ["Engineer", "Intern", "Manager", "All Done!"], 
+            choices: ["Engineer", "Intern", "Manager", "Exit"], 
             name: "role"
+        }
+        ])
+            .then (function(response){
+                console.log(response);
+            switch(response.role){
+                case "Engineer":
+                    makeEngineer(response.role);
+                break;
+
+                case "Manager" : 
+                    makeManager(response.role);
+                break;
+
+                case "Intern": 
+                    makeIntern(response.role);
+                break;
+            
+            case "Exit":
+                console.log("done!")
+                 makeDirectory(allEmployees);  
+                    return "done!";
+                };
+            });
+        };    
+        
+    
+
+
+//=============================
+//Functions
+//=============================
+
+function makeEngineer(){
+    inquirer
+        .prompt([
+            {   
+                type: "input",
+                message: "What is the Employee's Name?",
+                name: "Name"
             },
+            {   
+                type: "input",
+                message: "What is thier Employee ID?",
+                name: "Id"
+            },
+            {   
+                type: "input",
+                message: "What is their email?",
+                name: "Email"
+            }, 
+            {
+                type: "input",
+                message: "What is their GitHub Username?",
+                name: "gitHubUsername" 
+            },
+        ])
+        .then (function(engineerResponse){
+            let newEngineer = new Engineer(engineerResponse.Name, engineerResponse.Id, engineerResponse.Email, engineerResponse.gitHubUsername);
+            console.log(newEngineer);
+            allEmployees.push(newEngineer);
+            console.log(allEmployees);
+            newEmployeeSurvey();
+        })
+}
+
+function makeManager(){
+    inquirer
+    .prompt([ 
         {   
             type: "input",
             message: "What is the Employee's Name?",
@@ -36,76 +103,75 @@ function newEmployeeSurvey (){
             type: "input",
             message: "What is their email?",
             name: "Email"
+        },
+        {
+            type: "input",
+            message: "What is their office number?",
+            name: "officeNumber" 
         }
-        ])
+    ]) 
+    .then (function(managerResponse){
+        let newManager = new Manager (managerResponse.Name, managerResponse.Id, managerResponse.Email, managerResponse.officeNumber);
+        console.log(newManager);
+        allEmployees.push(newManager);
+        console.log(allEmployees);
+        newEmployeeSurvey();
+    })
+}
 
-            .then (function(response){
-                console.log(response);
-            switch(response.role){
-                case "Engineer":
-                inquirer
-                .prompt([ 
-                    {
-                        type: "input",
-                        message: "What is their GitHub Username?",
-                        name: "gitHubUsername" 
-                    },
-                ])
-                .then (function(engineerResponse){
-                    let newEngineer = new Engineer(response.Name, response.Id, response.Email, engineerResponse.gitHubUsername);
-                    console.log(newEngineer);
-                    allEmployees.push(newEngineer);
-                    console.log(allEmployees);
-                    newEmployeeSurvey();
-                })
-                break;
+function makeIntern(){
+    inquirer
+    .prompt([ 
+        {   
+            type: "input",
+            message: "What is the Employee's Name?",
+            name: "Name"
+        },
+        {   
+            type: "input",
+            message: "What is thier Employee ID?",
+            name: "Id"
+        },
+        {   
+            type: "input",
+            message: "What is their email?",
+            name: "Email"
+        },
+        {
+            type: "input",
+            message: "What School Do you attend?",
+            name: "school" 
+        }
+    ]) 
+    .then (function(internResponse){
+        let newIntern = new Intern (internResponse.Name, internResponse.Id, internResponse.Email, internResponse.school);
+        console.log(newIntern);
+        allEmployees.push(newIntern);
+        //console.log(allEmployees);
+        newEmployeeSurvey();
+    })
+}
 
-                case "Manager" : 
-                inquirer
-                .prompt([ 
-                    {
-                        type: "input",
-                        message: "What is their office number?",
-                        name: "officeNumber" 
-                    }
-                ]) 
-                .then (function(managerResponse){
-                    let newManager = new Manager (response.Name, response.Id, response.Email, managerResponse.officeNumber);
-                    console.log(newManager);
-                    allEmployees.push(newManager);
-                    console.log(allEmployees);
-                    newEmployeeSurvey();
-                })
-                break;
-
-                case "Intern": 
-                inquirer
-                .prompt([ 
-                    {
-                        type: "input",
-                        message: "What School Do you attend?",
-                        name: "school" 
-                    }
-                ]) 
-                .then (function(internResponse){
-                    let newIntern = new Intern (response.Name, response.Id, response.Email, internResponse.school);
-                    console.log(newIntern);
-                    allEmployees.push(newIntern);
-                    console.log(allEmployees);
-                    newEmployeeSurvey();
-                })
-                break;
-            
-
-            case "All Done!":
-                    
-                    render(allEmployees);  
-                    return "done!";
-                };
-            });
-        };    
-        
-
-newEmployeeSurvey();
+//=============================
 // fs the output directory, write the Team file to the the directory (create function asking if this directory exists) then render the team members to that path
+function makeDirectory(){
+    console.log("all employees" + JSON.stringify(allEmployees))
+    let allEmployeesArray= JSON.stringify(allEmployees)
+        if(!fs.existsSync(OUTPUT_DIR)){
+            createFile(allEmployeesArray);       
+            console.log("the directory exists")
+        } else {
+            createFile(JSON.stringify(allEmployees))
+        };
+};
 
+function createFile(allEmployees){
+    console.log("We are in the createFile Function" + allEmployees)
+    fs.writeFile(outputPath, allEmployees, function(err){
+        if (err) throw err; 
+    })
+}
+
+newEmployeeSurvey(); // run through survey
+
+ // need to put each type of employee in their own function and then the switch statement will call that function instead. 
